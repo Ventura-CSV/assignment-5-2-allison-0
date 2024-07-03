04668c290c31c465ca33267c6a18d7b3a8099c0c
allison-0 <allisonulrich00@gmail.com>
2024-07-02 22:18:52 -0700

    commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 def isPrime(num):
-    ##################################################
-    # make your code
-    ##################################################
-    return 0
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
 def primeNumbers(begin, end):
+    plist = []
     ##################################################
     # make your code
     ##################################################
